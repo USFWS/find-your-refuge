671120db31254b180fe8d174d9a38ba73596fcce
Roy Hewitt <roy_hewitt@fws.gov>
2020-02-28 14:57:11 -0500

    Add leaflet images; show poup on hover, remove link

--- src/js/icons.js
+++ src/js/icons.js
@@ -4,28 +4,28 @@ const blueMarker = new L.Icon({
   iconSize: [12, 12],
   iconAnchor: [5, 5],
   popupAnchor: [1, -5],
-  iconUrl: './images/blue.svg',
+  iconUrl: './images/svg/blue.svg',
 });
 
 const yellowMarker = new L.Icon({
   iconSize: [12, 12],
   iconAnchor: [5, 5],
   popupAnchor: [1, -5],
-  iconUrl: './images/yellow.svg',
+  iconUrl: './images/svg/yellow.svg',
 });
 
 const orangeMarker = new L.Icon({
   iconSize: [18, 18],
   iconAnchor: [8, 8],
   popupAnchor: [1, -8],
-  iconUrl: './images/orange.svg',
+  iconUrl: './images/svg/orange.svg',
 });
 
 const whiteMarker = new L.Icon({
   iconSize: [12, 12],
   iconAnchor: [5, 5],
   popupAnchor: [1, -5],
-  iconUrl: './images/white.svg',
+  iconUrl: './images/svg/white.svg',
 });
 
 module.exports = {
