5913808e56c77d638c84dce4ef0a024ce7eae30e
Roy Hewitt <roy_hewitt@fws.gov>
2020-03-02 13:10:04 -0500

    Display list of amenities in sidebar for selected refuge

--- src/js/templates/refuge.js
+++ src/js/templates/refuge.js
@@ -1,12 +1,29 @@
+const { getAmenityIcon } = require('../layers');
+
+const createAmenityItem = (amenity) => {
+  const icon = getAmenityIcon(amenity.properties.Category);
+  const coords = [...amenity.geometry.coordinates].reverse();
+  console.log(amenity);
+  return `
+    <li>
+      <button class="zoom-to-amenity" data-coordinates="${JSON.stringify(coords)}">
+        <img src="data:image/png;base64,${icon.symbol.imageData}" alt="${icon.label} icon" /> ${amenity.properties.Name}
+      </button>
+    </li>
+  `
+}
+
 const refugeTemplate = (refuges) => {
   const props = refuges[0].properties;
   console.log(props);
   return `
     <li>
       <h3>${props.OrgName} in ${props.State_Name}</h3>
-      <a href="${props.URL}" target="_blank">
-        <svg><use xlink:href="#world"></use></svg>
-      </a>
+      <p><a href="${props.URL}" target="_blank">Visit us on the web</a></p>
+      ${props.amenities.length ? `<h4>Amenities</h4>` : ''}
+      <ul class="amenities-list">
+        ${props.amenities.map(createAmenityItem).join('')}
+      </ul>
     </li>
   `
 };
