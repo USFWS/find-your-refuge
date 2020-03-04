62ba5a1bb424c25258a26c4a7360371dfd73b0c8
Roy Hewitt <roy_hewitt@fws.gov>
2020-03-04 13:25:59 -0500

    Find amenity by name + refuge name

--- src/js/AmenitiesService.js
+++ src/js/AmenitiesService.js
@@ -2,6 +2,14 @@ const { extentToLatLngBounds } = require('./helpers');
 
 const AMENITIES_URL = 'https://services.arcgis.com/QVENGdaPbd4LUkLV/arcgis/rest/services/FWS_National_Visitor_Service_Amenities_View/FeatureServer/0/query';
 
+const getAmenityByNameAndRefuge = (name, orgName) => {
+  const API_URL = `${AMENITIES_URL}?outFields=*&f=pgeojson&where=OrgName='${encodeURIComponent(orgName)}'+AND+Name='${encodeURIComponent(name)}'`;
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then((res) => res.features[0])
+    .catch(console.log);
+};
+
 const getAmenitiesByOrgName = (orgName) => {
   const API_URL = `${AMENITIES_URL}?outFields=*&f=pgeojson&where=OrgName='${orgName}'`;
   return fetch(API_URL)
@@ -10,10 +18,10 @@ const getAmenitiesByOrgName = (orgName) => {
 };
 
 const getAmenityById = (id) => {
-  const API_URL = `${AMENITIES_URL}?outFields=OrgName,Name&f=pgeojson&objectIds=${id}`;
+  const API_URL = `${AMENITIES_URL}?outFields=*&f=pgeojson&objectIds=${id}`;
   return fetch(API_URL)
     .then((res) => res.json())
-    .then((res) => res.features[0].properties)
+    .then((res) => res.features[0])
     .catch(console.log);
 };
 
@@ -28,5 +36,6 @@ const getAmenityBounds = (name, orgName) => {
 module.exports = {
   getAmenitiesByOrgName,
   getAmenityById,
-  getAmenityBounds
+  getAmenityBounds,
+  getAmenityByNameAndRefuge
 }
