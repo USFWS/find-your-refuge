af25ea528aebb5d08d1ecfeb921d7c0684b2fbe1
Roy Hewitt <roy_hewitt@fws.gov>
2020-03-05 11:45:38 -0500

    Update website URL, force HTTPS

--- src/js/templates/refuge.js
+++ src/js/templates/refuge.js
@@ -14,10 +14,11 @@ const createAmenityItem = (amenity) => {
 
 const refugeTemplate = (refuges) => {
   const props = refuges[0].properties;
+  console.log(props);
   return `
     <li>
       <h3>${props.OrgName} in ${props.State_Label}</h3>
-      <p><a href="${props.URL}" target="_blank">Visit us on the web</a></p>
+      <p><a href="${props.UrlStation.replace('http', 'https')}" target="_blank">Visit us on the web</a></p>
       ${props.amenities.length ? `<h4>Amenities</h4>` : ''}
       <ul class="amenities-list">
         ${props.amenities.map(createAmenityItem).join('')}
