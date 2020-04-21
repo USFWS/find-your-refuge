bb38329be22b766383e64010d8f02c6363a6d8e4
Roy Hewitt <roy_hewitt@fws.gov>
2020-04-21 13:50:26 -0400

    Include only public use amenities and exclude refuge easement and agreement polygons

--- src/js/layers.js
+++ src/js/layers.js
@@ -18,12 +18,14 @@ const refuges = esri.featureLayer({
   url: 'https://services.arcgis.com/QVENGdaPbd4LUkLV/arcgis/rest/services/FWSInterest_Simplified_Authoritative/FeatureServer/1',
   minZoom: 11,
   attribution: 'U.S. Fish and Wildlife Service',
+  where: `INTTYPE1 NOT IN ('E', 'A')`,
   onEachFeature: (feature, layer) => layer.on('click', () => emitter.emit('zoom:refugefeature', feature))
 });
 
 const amenities = esri.featureLayer({
   url: 'https://services.arcgis.com/QVENGdaPbd4LUkLV/arcgis/rest/services/FWS_National_Visitor_Service_Amenities_View/FeatureServer/0',
   minZoom: 11,
+  where: "Public_Use = 'Public Use'",
   onEachFeature: (feature, layer) => {
     layer.bindPopup(`<p>${feature.properties.Name}</p>`)
     layer.on('click', (e) => {
